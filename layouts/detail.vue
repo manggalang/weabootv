893ef23279ec98f8d66256b5a3fc84402a9b025a
manggalang <galangpastibisa@gmail.com>
2022-01-31 17:18:08 +0800

    create detail layout

---
+++ layouts/detail.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import AnimeService from "@/services/anime.service";
+const animeService = new AnimeService();
+const route = useRoute();
+
+const { data: animeData } = await useAsyncData("anime", () =>
+  animeService.getDetailAnime(Number(route.params.id))
+);
+</script>
+
+<template>
+  <div>
+    <Html>
+      <Head>
+        <Title>{{ animeData?.title ?? "Weabootv" }}</Title>
+      </Head>
+    </Html>
+    <Header />
+    <slot />
+    <Footer />
+  </div>
+</template>
+
+<style lang="scss">
+* {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+.main-content {
+  margin-top: 84px;
+  min-height: 69.6vh;
+  padding: 30px 0;
+  position: relative;
+
+  @media screen and (max-width: 768px) {
+    padding-top: 119px;
+  }
+}
+
+.container-large {
+  max-width: 1260px;
+  margin: auto;
+  position: relative;
+
+  @media only screen and(max-width: 1260px) {
+    max-width: 1180px;
+  }
+
+  @media screen and(max-width: 1180px) {
+    max-width: 1024px;
+  }
+
+  @media screen and(max-width:1024px) {
+    max-width: 960px;
+  }
+
+  @media screen and(max-width: 960px) {
+    max-width: 768px;
+  }
+
+  @media screen and (max-width: 768px) {
+    max-width: 576px;
+  }
+
+  @media screen and (max-width: 576px) {
+    max-width: 100%;
+    padding: 0 16px;
+  }
+}
+
+.container-medium {
+  max-width: 970px;
+  margin: auto;
+  position: relative;
+}
+</style>
